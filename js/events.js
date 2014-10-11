715bd6444cf34691db2e48e434de0cce496c2e68
dudu06 <jeremidudu@gmail.com>
2014-10-11 12:38:33 -0700

    Pictures are now fully responsive. Sound bites play on click.

---
+++ js/events.js
@@ -0,0 +1,30 @@
+var jello = new Audio();
+jello.src = "audio/billcosby.mp3";
+
+var time = new Audio();
+time.src = "audio/mchammer.mp3";
+
+var hitch = new Audio();
+hitch.src = "audio/willsmith.mp3";
+
+var tony = new Audio();
+tony.src = "audio/tonymontana.mp3";
+
+var bball = new Audio();
+bball.src = "audio/charlesbarkley.mp3";
+
+var power = new Audio();
+power.src = "audio/powerrangers.mp3";
+
+var theman = new Audio();
+theman.src = "audio/jamesbrown.mp3";
+
+var yeezy = new Audio();
+yeezy.src = "audio/kanye.mp3";
+
+var snl = new Audio();
+snl.src = "audio/willferrell.mp3";
+
+var ruffryder = new Audio();
+ruffryder.src = "audio/dmx.mp3";
+
